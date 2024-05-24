a47a3a710b2dcd5b5f4e18693a39e8bb6ed02bd0
Mitesh Appstonelab <mitesh.viras@dev.appstonelab.com>
2024-05-24 10:42:28 +0530

    first commit

---
+++ database/migrations/1716527358508_create_rss_feeds_table.ts
@@ -0,0 +1,20 @@
+import { BaseSchema } from '@adonisjs/lucid/schema'
+
+export default class extends BaseSchema {
+  protected tableName = 'rss_feeds'
+
+  async up() {
+    this.schema.createTable(this.tableName, (table) => {
+      table.increments('id')
+      table.string('source_name').nullable()
+      table.string('source_url', 1000).nullable()
+      table.dateTime('recall_at').nullable()
+      table.timestamp('created_at')
+      table.timestamp('updated_at')
+    })
+  }
+
+  async down() {
+    this.schema.dropTable(this.tableName)
+  }
+}
